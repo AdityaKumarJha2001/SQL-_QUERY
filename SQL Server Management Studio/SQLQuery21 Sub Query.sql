357c951d97d8fcceb81554ffe3aa90743d9ce974
AdityaKumarJha2001 <89518972+AdityaKumarJha2001@users.noreply.github.com>
2021-09-11 21:15:34 +0530

    Update SQLQuery21 Sub Query.sql

--- SQL Server Management Studio/SQLQuery21 Sub Query.sql
+++ SQL Server Management Studio/SQLQuery21 Sub Query.sql
@@ -1,13 +1,14 @@
---1.Sub Query or inner query or a Nested query within another SQL query and embedded within the WHERE clause.
---2.A Subquery is used to return data that will be used in the main query as a condition to further resstrict the data to be retrived.
---3 There are a few rules that subqueries must follow
---	# Subqueries must be enclosed within parenthese ().
---	# A subquery can have only one column in the SELECT clause,unless multiple columns are in the main query for the subquery to compare its selected columns.
---  # An ORDER BY command cannot be used in a subquery, although the main query can use an ORDER BY. 
---  # Subquery that return more than one row can only be used with multiple value operators such as the IN operator.
---	# We can use subquery with two tables also.
---	# We can also update and delete with subquery.
---	#  We can display particular columns in outer query with subquery. 
+/*1.Sub Query or inner query or a Nested query within another SQL query and embedded within the WHERE clause.
+2.A Subquery is used to return data that will be used in the main query as a condition to further resstrict the data to be retrived.
+3 There are a few rules that subqueries must follow
+	# Subqueries must be enclosed within parenthese ().
+	# A subquery can have only one column in the SELECT clause,unless multiple columns are in the main query for the subquery to compare its selected columns.
+  # An ORDER BY command cannot be used in a subquery, although the main query can use an ORDER BY. 
+  # Subquery that return more than one row can only be used with multiple value operators such as the IN operator.
+	# We can use subquery with two tables also.
+	# We can also update and delete with subquery.
+	#  We can display particular columns in outer query with subquery. 
+*/
 use Aditya_123
 
 Create table MyEmployee
