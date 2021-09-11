8cdba457839ab7527a43be1c7cad133737d81496
AdityaKumarJha2001 <89518972+AdityaKumarJha2001@users.noreply.github.com>
2021-09-11 21:29:47 +0530

    Update SQLQuery25 Select Into statement.sql

--- SQL Server Management Studio/SQLQuery25 Select Into statement.sql
+++ SQL Server Management Studio/SQLQuery25 Select Into statement.sql
@@ -1,10 +1,11 @@
---	The SELECT INTO statement in sql server, selects the data from one table and inserts it into a new table.
---	Mostly it used to create backups of the table.
---	We can copy all the rows and columns from an existing table into a new table.
---	We can copy selected columns into a new table.
---	We can copy selected rows into a new table.
---	Copy columns from 2 or more tables into a new tables (using joins), all columns or selected columns.
---	We can copy data from one database table and the paste it to another database new table.
+/*	The SELECT INTO statement in sql server, selects the data from one table and inserts it into a new table.
+  	Mostly it used to create backups of the table.
+  	We can copy all the rows and columns from an existing table into a new table.
+  	We can copy selected columns into a new table.
+  	We can copy selected rows into a new table.
+  	Copy columns from 2 or more tables into a new tables (using joins), all columns or selected columns.
+  	We can copy data from one database table and the paste it to another database new table.
+*/
 
 use Aditya_123
 
