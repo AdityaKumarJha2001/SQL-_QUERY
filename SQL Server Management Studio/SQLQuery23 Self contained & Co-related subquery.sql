3121f901f43843b5f97af9030fc9e067e5df16da
AdityaKumarJha2001 <89518972+AdityaKumarJha2001@users.noreply.github.com>
2021-09-11 21:27:27 +0530

    Update SQLQuery23 Self contained & Co-related subquery.sql

--- SQL Server Management Studio/SQLQuery23 Self contained & Co-related subquery.sql
+++ SQL Server Management Studio/SQLQuery23 Self contained & Co-related subquery.sql
@@ -1,13 +1,15 @@
---  SELF CONTAINED SUBQUERIES 
---		# These queries are written as standalone queries, without any dependencies on the outer query.
---		# A self-contained subquery is processed once when the outer query runs and passes its results to the outer query
+/*  SELF CONTAINED SUBQUERIES 
+		# These queries are written as standalone queries, without any dependencies on the outer query.
+		# A self-contained subquery is processed once when the outer query runs and passes its results to the outer query
 
--- CO-RELATED SUB QUERIES
---		# There queries reference one or more columns from the outer query and therefore, depend on the outer query.
---		# Corelated subqueries cannot be run separately from the outer query.
+ CO-RELATED SUB QUERIES
+		# There queries reference one or more columns from the outer query and therefore, depend on the outer query.
+		# Corelated subqueries cannot be run separately from the outer query.
 
--- NOTE:- We can use scalar aggregate functions in subqueries because where clause don't allow us to use aggregate functions with where clause.
---		  Self-contained or Co-related subqueries both can be scalar and multivalued subqueries.
+ NOTE:- We can use scalar aggregate functions in subqueries because where clause don't allow us to use aggregate functions with where clause.
+		  Self-contained or Co-related subqueries both can be scalar and multivalued subqueries.
+*/
+
 use Aditya_123
 
 select * from MyEmployee
