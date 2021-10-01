fd806d09d271982c099c5d43a55b5c88597531a8
AdityaKumarJha2001 <89518972+AdityaKumarJha2001@users.noreply.github.com>
2021-10-01 15:56:00 +0530

    Update SQLQuery41 GUID (GLOBALLY UNIQUE IDENTIFIER ) IN TABLE IN SQL SERVER.sql

--- SQL Server Management Studio/SQLQuery41 GUID (GLOBALLY UNIQUE IDENTIFIER ) IN TABLE IN SQL SERVER.sql
+++ SQL Server Management Studio/SQLQuery41 GUID (GLOBALLY UNIQUE IDENTIFIER ) IN TABLE IN SQL SERVER.sql
@@ -7,34 +7,6 @@
 
 use Aditya_123
 
-create table Customer_Hyderabad (Ch_Id int primary key identity, Ch_Name varchar(50))
-
-insert into Customer_Hyderabad values ('Abc')
-insert into Customer_Hyderabad values ('Pqr')
-insert into Customer_Hyderabad values ('Jkl')
-
-select * from Customer_Hyderabad
-
-create table Customer_Karanchi (Ch_Id int primary key identity, Ch_Name varchar(50))
-
-insert into Customer_Karanchi values ('Hju')
-insert into Customer_Karanchi values ('Jo')
-insert into Customer_Karanchi values ('Ko')
-
-select * from Customer_Karanchi
-
-create table Customers (C_Id int primary key, C_NAme varchar(50))
-
-select * from Customers
-
-insert into Customers
-Select * from Customer_Hyderabad
-union all
-select * from Customer_Karanchi
-
-
-                                              --GUID
-
 create table Customer_Hyderabad1 (Ch_Id uniqueidentifier primary key default newid(), Ch_Name varchar(50))
 
 insert into Customer_Hyderabad1 values (default,'Abc')
